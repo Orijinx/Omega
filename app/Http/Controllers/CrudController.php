036bd2b14bb0b335df1ea4ec985c03ab4e0cb126
Vladislav <orijinx.ifno@gmail.com>
2021-06-29 13:28:42 +0300

    Описаны CUD методы для должностей

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\department;
+use App\Models\position;
+use Illuminate\Support\Facades\Auth;
 
 class CrudController extends Controller
 {
@@ -41,6 +43,49 @@ class CrudController extends Controller
             if (isset($dep->name)) {
                 $dep->name = $req->name;
                 $dep->save();
+                return back()->with("status", "Успешно!");
+            } else {
+                return back()->with("status", "Неудачно!");
+            }
+        } else {
+            return back()->with("status", "Неудачно!");
+        }
+    }
+    /////////////////////////////////////////////////////////////
+    //Добавление должности
+    public function AddPosition(Request $req)
+    {
+        $pos = new position();
+        if (isset($req->name) && (Auth::user()->rights >= 1)) {
+            $pos->name = $req->name;
+            $pos->save();
+            return back()->with("status", "Успешно!");
+        } else {
+            return back()->with("status", "Неудачно!");
+        }
+    }
+    //Удаление отедал
+    public function DelPosition(Request $req)
+    {
+        if (isset($req->id) && (Auth::user()->rights > 1)) {
+            if (is_array($req->id)) {
+                position::destroy($req->id);
+            } else {
+                position::findOrFail($req->id)->delete();
+            }
+            return back()->with("status", "Успешно!");
+        } else {
+            return back()->with("status", "Неудачно!");
+        }
+    }
+    //Удаление отедал
+    public function UpdPosition(Request $req)
+    {
+        if (isset($req->id) && (Auth::user()->rights > 1)) {
+            $pos = position::where("id", $req->id);
+            if (isset($pos->name)) {
+                $pos->name = $req->name;
+                $pos->save();
                 return back()->with("status", "Успешно!");
             } else {
                 return back()->with("status", "Неудачно!");
