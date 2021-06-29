fd37e0a19f6930be5b50b94c6120a95fbe392506
Vlad <vladonnx@mail.ru>
2021-06-29 04:11:34 +0300

    Обновлена связь для модели должностей

--- app/Models/department.php
+++ app/Models/department.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\department_conn;
+use App\Models\User;
 
 class department extends Model
 {
     use HasFactory;
     public function users(){
-        return $this->belongsToMany(department_conn::class,'departmentconns','dep_id','user_id')->withTimestamps();
+        return $this->belongsToMany(User::class,'department_conns','dep_id','user_id');
     }
 }
